f3a2c792ef60e1bfc99a3488d9bb98fa8bd571cd
Victoria Brown <victoriabrown0710@gmail.com>
2016-07-01 18:49:28 -0500

    Beginning of class ReverseAString

---
+++ ReverseAString.java
@@ -0,0 +1,16 @@
+/*
+Program Name:   ReverseAString
+Progammer:      Victoria Brown
+Date:           July 2016
+Purpose:        Enter a string and the program will reverse it and print it
+                out.
+*/
+
+import java.util.Scanner;
+
+public class ReverseAString {
+
+  public static void main(String[] args) {
+    
+  }
+}
