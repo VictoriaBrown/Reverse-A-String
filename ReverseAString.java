f74e68b8b74c6b546c4cf202e3e3886b397e005c
Victoria Brown <victoriabrown0710@gmail.com>
2016-07-02 15:01:42 -0500

    Finalized ReverseAString

--- ReverseAString.java
+++ ReverseAString.java
@@ -11,6 +11,24 @@ import java.util.Scanner;
 public class ReverseAString {
 
   public static void main(String[] args) {
-    
+
+    Scanner input = new Scanner(System.in);
+    String phrase;
+    StringBuilder input1 = new StringBuilder();
+
+    // Get the user's  string
+    System.out.println("Please enter the phrase you wish to be reversed: ");
+    phrase = input.nextLine();
+
+    // Change string into StringBuilder
+    input1.append(phrase);
+
+    // Reverse the phrase
+    input1 = input1.reverse();
+
+    // Print out the phrase.
+    for (int i = 0; i < input1.length(); i++ ) {
+      System.out.print(input1.charAt(i));
+    }
   }
 }
